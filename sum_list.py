6872f48e637925d9137456fd71b3c6ff06d56e14
Barry477 <masigabarrack@gmail.com>
2025-04-26 22:52:01 +0300

    Create sum_list.py
    Add function to sum all elements in a list

---
+++ sum_list.py
@@ -0,0 +1,10 @@
+# Function to sum all elements in a list 
+def sum_list(numbers):
+  total = 0
+  for sum in numbers:
+    total += num # Add each number to total
+return total
+
+# Exanple usage
+my_list = [1, 2, 3, 4, 5]
+print ("Sum of list;", sum_list(my_list))
