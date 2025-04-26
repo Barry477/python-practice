a8a5f20b14bc9f5f5ded42049276f9e67a18fe08
Barry477 <masigabarrack@gmail.com>
2025-04-26 23:34:26 +0300

    Update factorial_recursive.py
    Add recursive factorial function

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -1,4 +1,6 @@
 # Function to compute factorial using recursion 
+# use a base case: if n is 0 or 1, return 1
+# otherwise, return n times factorial of (n-1)
 def factorial_recursion(n):
   if n == 0 or n==1:
     return 1 # Base case
