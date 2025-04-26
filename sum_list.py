63ad6b223bf2025597f5254ffa61c79d9494134d
Barry477 <masigabarrack@gmail.com>
2025-04-26 23:22:40 +0300

    Update sum_list.py
    Add function to sum all elements in a list

--- sum_list.py
+++ sum_list.py
@@ -1,4 +1,11 @@
 # Function to sum all elements in a list 
+# I need to calculate the tota of all numbers in a list 
+# My plan:
+# Start with total = 0
+# Go through each number using a for loop
+# Keep adding each number to the total
+# Finally, return the total sum
+
 def sum_list(numbers):
   total = 0
   for sum in numbers:
