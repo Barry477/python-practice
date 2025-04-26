eca0db77fe00191f8d9690070507f732b7565eb2
Barry477 <masigabarrack@gmail.com>
2025-04-26 23:28:04 +0300

    Update even_or_odd.py
    Add function to check if a number is even or odd

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,4 +1,9 @@
 # Function to check if a number is even or odd
+# I need to check if a number is even or odd
+# Use modulus operator (%) to divide by 2
+# If remainder is 0, its even
+# Otherwise, its odd
+
 def check_even_odd(number):
   if number % 2 == 0:
     return "Even"
